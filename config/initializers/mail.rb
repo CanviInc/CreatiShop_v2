3ecbb08f4c5ec0d1582ea22f57831e5f5628d0af
Pawan <pawanchawda.ruby@gmail.com>
2014-07-24 15:14:21 +0530

    error

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@
 # This is needed for using sendgrid on heroku
 ENV['SENDGRID_USERNAME'] = "jai.n@cisinlabs.com"
 ENV['SENDGRID_PASSWORD'] = "download123"
-ActionMailer::Base.mailer_sender = "no-reply@gmail.com"
+# ActionMailer::Base.mailer_sender = "no-reply@gmail.com"
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
