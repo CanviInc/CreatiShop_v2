25fd2990dc0582288cc9a259b49d16ac0755b6bf
Pawan <pawanchawda.ruby@gmail.com>
2014-07-24 15:11:43 +0530

    sendgrid user add

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,8 @@
 
 # This is needed for using sendgrid on heroku
-
+ENV['SENDGRID_USERNAME'] = "jai.n@cisinlabs.com"
+ENV['SENDGRID_PASSWORD'] = "download123"
+ActionMailer::Base.mailer_sender = "no-reply@gmail.com"
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
