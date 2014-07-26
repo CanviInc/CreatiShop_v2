42609605e8be605fef81950e4990a3b545487440
Pawan <pawanchawda.ruby@gmail.com>
2014-07-26 20:41:52 +0530

    changes

--- app/controllers/shopping/base_controller.rb
+++ app/controllers/shopping/base_controller.rb
@@ -54,7 +54,7 @@ class Shopping::BaseController < ApplicationController
 
   def find_or_create_order
     return @session_order if @session_order
-    if session[:order_id]
+    unless session[:order_id]
       @session_order = current_user.orders.include_checkout_objects.find(session[:order_id])
       create_order if !@session_order.in_progress?
     else
